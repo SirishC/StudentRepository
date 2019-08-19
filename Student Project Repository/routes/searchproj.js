bde5de5814305ff5f2f648dbbd658cbccce7d2dc
ravibalg-aj <aravibalaji@gmail.com>
2019-08-19 21:45:19 +0530

    Added (Only if user exists Page viewing allowed!)

--- Student Project Repository/routes/searchproj.js
+++ Student Project Repository/routes/searchproj.js
@@ -3,6 +3,18 @@ var router = express.Router();
 var projDetails = require("../models/projDetails");
 
 
+function loginReq(req, res, next) {
+    if (req.session && req.session.userId) {
+        return next();
+    } else {
+        var err = new Error('You must be logged in to view this page.');
+        err.status = 401;
+        return next(err);
+    }
+}
+
+router.use(loginReq);
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('searchproj', {title: 'Search Projects', data: []});
--- Student Project Repository/routes/viewproj.js
+++ Student Project Repository/routes/viewproj.js
@@ -2,12 +2,24 @@ var express = require('express');
 var router = express.Router();
 var projDetails = require("../models/projDetails");
 
+function loginReq(req, res, next) {
+    if (req.session && req.session.userId) {
+        return next();
+    } else {
+        var err = new Error('You must be logged in to view this page.');
+        err.status = 401;
+        return next(err);
+    }
+}
+
+router.use(loginReq);
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     projId = req.query.projId;
 
-    console.log("Requing for Proj : " + projId);
+    console.log("Requesting for Proj : " + projId);
     var query = {_id: projId};
     projDetails.find(query)
         .then(proj => {
